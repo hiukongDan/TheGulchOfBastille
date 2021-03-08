a01d2cff776f93597666104ad1e665643554af79
hiukongDan <ifsoboy9148@hotmail.com>
2021-03-09 01:59:53 +0800

    ready for backup

---
+++ Assets/Script/Environment/Ladder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+
+        }
+    }
+}
