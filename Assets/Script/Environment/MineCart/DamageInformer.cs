6f571e2ad58bfe73c508fbebbc69a60695c5f1aa
hiukongDan <ifsoboy9148@hotmail.com>
2021-03-30 18:03:43 +0800

    prepare to fight the demon

---
+++ Assets/Script/Environment/MineCart/DamageInformer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageInformer : MonoBehaviour
+{
+    public MonoBehaviour DamageInformationDealer;
+    void Damage(CombatData data){
+        DamageInformationDealer.SendMessage("Damage", data);
+    }
+}
---
+++ Assets/Script/Environment/MineCart/Minecart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minecart : MonoBehaviour
+{
+    
+    public void Damage(CombatData data){
+        if(data.from.tag == "Player"){
+            
+        }
+    }
+}
