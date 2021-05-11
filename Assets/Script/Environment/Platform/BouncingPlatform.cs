ffe9919e4ad6a84848d395df9471c4d2f98d9c61
FranMukuro <FranMukuro6926@gmail.com>
2021-05-11 17:48:09 +0800

    complete first part platformer design

--- Assets/Script/Environment/Platform/BouncingPlatform.cs
+++ Assets/Script/Environment/Platform/BouncingPlatform.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BouncingPlatform : MonoBehaviour
 {
+	public float distance = 1.2f;
+	public float duration = 4f;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        isBouncingMovement = false;
     }
 
     // Update is called once per frame
@@ -15,15 +18,38 @@ public class BouncingPlatform : MonoBehaviour
     {
         
     }
+	
+	protected bool isBouncingMovement;
 
+	/*
     void OnCollisionEnter2D(Collision2D collision) {
-        //StartCoroutine(bounce());    
-        Debug.Log("contact count: " + collision.contactCount);
-        Debug.Log(collision.GetContact(0).point);
+		BoxCollider2D selfCollider = transform.GetComponent<BoxCollider2D>();
+		if(!isBouncingMovement && Gulch.Math.AlmostGreater(collision.GetContact(0).point.y, 
+							transform.position.y + selfCollider.size.y/2)){
+			isBouncingMovement = true;
+			StartCoroutine(bounce());
+		}
     }
+	*/
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(!isBouncingMovement){
+			isBouncingMovement = true;
+			StartCoroutine(bounce());
+		}
+	}
 
     IEnumerator bounce(){
-        Debug.Log("Bounce at " + Time.fixedTime);
-        yield return null;
+		var curve = new Gulch.BouncingCurve_Instance_A(distance, duration);
+		float time = 0f;
+		Vector2 originPosition = transform.position;
+		while(!curve.IsEnd(time)){
+			time += Time.deltaTime;
+			transform.position = new Vector2(originPosition.x, 
+				originPosition.y + curve.GetValue(time));
+			yield return new WaitForEndOfFrame();
+		}
+		
+		isBouncingMovement = false;
     }
 }
---
+++ Assets/Script/Helper/BouncingCurve.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Gulch{
+	
+public abstract class BouncingCurve
+{
+	/*
+		The curve traverse from t=0 to t=duration.
+		and is manually adjusted in such convenience.
+	*/
+	protected Func<float, float> curveFunc;
+	protected float duration;
+	
+    public BouncingCurve(float duration){
+		this.duration = duration;
+	}
+	
+	public virtual Func<float, float> GetCurve(){
+		return this.curveFunc;
+	}
+	
+	public virtual float GetValue(float t){
+		return curveFunc(t);
+	}
+	
+	public virtual bool IsEnd(float t) => t >= this.duration;
+}
+
+public class BouncingCurve_Instance_A : BouncingCurve
+{
+	/*
+		Please using y=\frac{k}{b^{3}}x\left(x-b\right)^{3}
+	    in desmos.com/calculator or some equivalent software 
+		to see the curve before using them
+	*/
+	public BouncingCurve_Instance_A(float k=2.5f, float b=9.0f) : base(b){
+		this.curveFunc = (t) => k/b/b/b * t * (t - b) * (t - b) * (t - b);
+	}
+	
+}
+
+};
