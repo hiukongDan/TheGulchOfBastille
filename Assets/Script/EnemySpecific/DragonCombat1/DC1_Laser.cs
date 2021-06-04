82005809f9a379e222bffef56f69a6b83528f50e
hiukongDan <ifsoboy9148@hotmail.com>
2021-06-04 17:16:19 +0800

    add  laser beam ball in the mouse

--- Assets/Script/EnemySpecific/DragonCombat1/DC1_Laser.cs
+++ Assets/Script/EnemySpecific/DragonCombat1/DC1_Laser.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class DC1_Laser : MonoBehaviour
 {
     public GameObject beamDustPref;
+    public GameObject BeamObject;
     public LayerMask whatIsGround;
     public LayerMask whatIsPlayer;
     public float groundDetectLength = 4f;
+
+
     private Animator animator;
     private CombatData combatData;
     private Vector2 lastGroundHitPos;
@@ -64,7 +67,11 @@ public class DC1_Laser : MonoBehaviour
 
     void CheckPlayerHit(){
         float angle = Vector2.SignedAngle(transform.up, Vector2.up);
-        SpriteRenderer sr = GetComponentInChildren<SpriteRenderer>();
+        SpriteRenderer sr = BeamObject.GetComponent<SpriteRenderer>();
+        if(sr.sprite == null){
+            return;
+        }
+
         Collider2D collider = Physics2D.OverlapBox(transform.position - transform.up * groundDetectLength/2,
                                                    new Vector2(sr.sprite.rect.width/sr.sprite.pixelsPerUnit, groundDetectLength),
                                                    angle,
