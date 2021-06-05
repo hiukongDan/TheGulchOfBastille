85876cd1bfaa4213f29541f57fabaec8285cdc1a
hiukongDan <ifsoboy9148@hotmail.com>
2021-06-05 14:48:07 +0800

    add barrel animations

---
+++ Assets/Script/Loot/Barrel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    public float MaxHp = 100;
+    private float currentHp = 0;
+    public List<Sprite> stateSprites;
+    private int currentState;
+
+    void Reset(){
+        MaxHp = currentHp;
+    }
+    void Update()
+    {
+        
+    }
+
+    void Damage(CombatData data){
+
+    }
+}
