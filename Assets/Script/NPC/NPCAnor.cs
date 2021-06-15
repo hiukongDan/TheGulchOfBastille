2a435cafb375716c1699ebc70d198cc90689aefb
hiukongDan <ifsoboy9148@hotmail.com>
2021-06-15 11:33:55 +0900

    add all weapon to map

---
+++ Assets/Script/NPC/NPCAnor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnor : NPCConversationHandler
+{
+    public override void OnEndInteraction()
+    {
+        // if the first conversation, required abandoned key
+        if(npcConversation == npcConversations[0]){
+            if(!player.playerRuntimeData.playerStock.weaponStock.Contains(new ItemData.WeaponRuntimeData(ItemData.Weapon.Sunlight_Stick, 0))){
+                Loot.OnPickUpLoot(player, ItemData.Weapon.Sunlight_Stick, 0);
+            }
+        }
+
+        base.OnEndInteraction();
+    }
+}
